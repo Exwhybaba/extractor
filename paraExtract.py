95c662e93e284cdd6dd54617cf84ec4e6074b60f
Oyelayo Seye <seyeoyelayo@gmail.com>
2024-02-09 03:44:17 +0100

    Update paraExtract.py

--- paraExtract.py
+++ paraExtract.py
@@ -1,19 +1,20 @@
 import pandas as pd
 import streamlit as st
 import spacy
-from pdfReader import extract
 import os
 
-
-
+# Function to install SpaCy model if not already installed
+def install_spacy_model():
+    if "en_core_web_sm" not in spacy.util.get_installed_models():
+        os.system("python -m spacy download en_core_web_sm")
 
 # Function to extract entities and save as CSV
 def entity(text, filename):
     current_question = 1
     question = []
-    alpha = ['A.', 'B.', 'C.', 'D.', 'E.', 'F.', 'G.', 'H.', 'I.', 'J.', 'K.', 
+    alpha = ['A.', 'B.', 'C.', 'D.', 'E.', 'F.', 'G.', 'H.', 'I.', 'J.', 'K.',
              'L.', 'M.', 'N.', 'O.', 'P.', 'Q.', 'R.', 'S.', 'T.', 'U.', 'V.', 'W.', 'X.', 'Y.', 'Z.',
-            'a.', 'b.', 'c.', 'd.', 'e.', 'f.', 'g.', 'h.', 'i.', 'j.', 'k.', 'l.', 'm.', 'n.', 'o.', 
+             'a.', 'b.', 'c.', 'd.', 'e.', 'f.', 'g.', 'h.', 'i.', 'j.', 'k.', 'l.', 'm.', 'n.', 'o.',
              'p.', 'q.', 'r.', 's.', 't.', 'u.', 'v.', 'w.', 'x.', 'y.', 'z.']
     for line in text:
         line = line.strip()  # Remove leading and trailing whitespaces
@@ -29,7 +30,7 @@ def entity(text, filename):
     nlp = spacy.load("en_core_web_sm")
     text_combined = '\n'.join(text)
     doc = nlp(text_combined)
-    named_entities = ["STUDY", "FORM", "CENTER", "PATIENT", "NAMECODE", "FORMCODE", "DATECOMP", "WEEK"] 
+    named_entities = ["STUDY", "FORM", "CENTER", "PATIENT", "NAMECODE", "FORMCODE", "DATECOMP", "WEEK"]
     # Initialize a dictionary to store extracted entities
     extracted_entities = {entity: [] for entity in named_entities}
 
@@ -40,16 +41,16 @@ def entity(text, filename):
 
     # Create a list of strings representing extracted entities without the colon
     entity_list = [entity for entity in extracted_entities.keys()]
-    
+
     # Extend entity_list with questions
     entity_list.extend(question)
-    
+
     # Create a DataFrame with columns as entity_list
     df = pd.DataFrame(columns=entity_list)
-    
+
     # Add data to the DataFrame (here, all cells are filled with 'NA')
     df.loc[0] = ['NA'] * len(entity_list)
-    
+
     # Save DataFrame to CSV with filename based on the uploaded file name
     df.to_csv(f'extract_{filename}.csv', index=False)
 
@@ -58,16 +59,19 @@ def main():
     st.set_page_config(page_title="PDF Text Extractor", layout="wide")
     st.title("PDF Text Extractor")
 
+    # Install SpaCy model if not already installed
+    install_spacy_model()
+
     # Allow user to upload a PDF file
     uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"], help="Please upload a PDF file.")
 
     if uploaded_file is not None:
         # Process the uploaded file and extract text
         text = extract(uploaded_file)
-        
+
         # Extract filename
         filename = uploaded_file.name.split('.')[0]  # Extract filename without extension
-        
+
         # Display the extracted text
         st.header("Extracted Text:")
         for line in text:
@@ -79,7 +83,9 @@ def main():
             entity(text, filename)
             # Provide download link
             with open(f'extract_{filename}.csv', 'rb') as f:
-                st.download_button(label=f'Download {filename}.csv', data=f, file_name=f'extract_{filename}.csv', mime='text/csv')
+                st.download_button(label=f'Download {filename}.csv', data=f, file_name=f'extract_{filename}.csv',
+                                   mime='text/csv')
+
 
 if __name__ == "__main__":
     main()
